589d5c69d71a7997f06d57d7846ba1d990bfc6cd
6wanted9 <44305361+6wanted9@users.noreply.github.com>
2021-10-29 19:21:51 +0000

    Deploying to heroku from  @ f0bab371b48e2e6c459edb58c2ac9224b6dab1a8 🚀

---
+++ Models/BroAndSis.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Documents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace task4.Models
+{
+    public static class BroAndSis
+    {
+        private static string bro;
+        private static string sis;
+        private static string lastMessage;
+        private static string lastUser;
+        private static string timeOfLastMsg;
+        public static string Bro
+        {
+            get
+            {
+                return bro;
+            }
+        }
+        public static string Sis
+        {
+            get
+            {
+                return sis;
+            }
+        }
+        public static string LastMessage
+        {
+            get
+            {
+                return lastMessage;
+            }
+        }
+        public static string LastUser
+        {
+            get
+            {
+                return lastUser;
+            }
+        }
+        public static string TimeOfLastMsg
+        {
+            get
+            {
+                return timeOfLastMsg;
+            }
+        }
+
+        static BroAndSis()
+        {
+            bro = "0";
+            sis = "0";
+        }
+        public static void BroClick(string User)
+        {
+            bro = Convert.ToString(Convert.ToInt32(bro) + 1);
+            lastMessage = "Bro!";
+            timeOfLastMsg = DateTime.UtcNow.ToLocalTime().ToShortTimeString();
+            lastUser = User;
+        }
+        public static void SisClick(string User)
+        {
+            sis = Convert.ToString(Convert.ToInt32(sis) + 1);
+            lastMessage = "Sis!";
+            timeOfLastMsg = DateTime.UtcNow.ToLocalTime().ToShortTimeString();
+            lastUser = User;
+        }
+    }
+}
