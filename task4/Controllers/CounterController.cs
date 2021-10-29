9bd2d5559fc1ba332bc6152c7c1407cd426f0ff4
WaNTeD <dkatanov2002@gmail.com>
2021-10-29 20:56:12 +0300

    task4 upd

---
+++ task4/Controllers/CounterController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using task4.Models;
+using static task4.Models.BroAndSis;
+
+namespace task4.Controllers
+{
+    public class CounterController : Controller
+    {
+        public void BroCL(string User)
+        {
+
+            BroClick(User);
+            Response.Redirect("/");
+        }
+        public void SisCL(string User)
+        {
+            SisClick(User);
+            Response.Redirect("/");
+        }
+    }
+}
